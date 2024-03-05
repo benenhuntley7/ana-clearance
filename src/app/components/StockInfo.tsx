243e2360d425e9a343d2187816aaeb394c7d8b90
Benen Huntley <benen.huntley@gmail.com>
2024-03-05 13:25:28 +1030

    updated loading spinners

--- src/app/components/StockInfo.tsx
+++ src/app/components/StockInfo.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { setPriced } from "../functions/supabase_functions";
+import { LoadingPage } from "./loadingSpinner";
 
 interface StoreDataProps {
   storeData: any[]; // Adjust the type accordingly
@@ -66,95 +67,56 @@ export const StockInfo = ({ storeData, setStoreData, selectedDepartment }: Store
 
   return (
     <>
-      <div className="flex justify-center mt-4">
-        <form className="flex">
-          <button
-            className={`btn btn-sm md:btn-md btn-outline me-4 ${sortedBy === "cost" ? "btn-active" : null}`}
-            onClick={sortByCost}
-          >
-            Sort By SOH@Cost
-          </button>
-          <button className="btn btn-sm md:btn-md btn-outline me-4 " onClick={sortByAge}>
-            Sort By Age
-          </button>
-          <button
-            className={`btn btn-sm md:btn-md btn-outline me-4 ${sortedBy === "soh" ? "btn-active" : null}`}
-            onClick={sortBySOH}
-          >
-            Sort By SOH
-          </button>
-        </form>
-      </div>
-      <div className="mt-5 px-2 md:px-40">
-        <p className="p-2">{storeData.length} clearance lines</p>
-        <div className="overflow-x-auto">
-          {/* Display table on medium or larger screens */}
-          <table className="hidden md:table table-zebra-zebra">
-            <thead>
-              <tr>
-                <th>Article</th>
-                <th>Description</th>
-                <th>MAP</th>
-                <th>SOH@Cost</th>
-                <th>RRP</th>
-                <th>Z-Status</th>
-                <th>SOH</th>
-                <th>Age</th>
-                <th>Priced</th>
-              </tr>
-            </thead>
-            <tbody>
-              {storeData.map((row, index) => (
-                <tr key={index}>
-                  <td>{row.article}</td>
-                  <td>{row.description}</td>
-                  <td>{CurrencyFormatter.format(row.map)}</td>
-                  <td>{CurrencyFormatter.format(row.cost)}</td>
-                  <td>{CurrencyFormatter.format(row.rrp)}</td>
-                  <td>{row.z_status}</td>
-                  <td>{row.soh}</td>
-                  <td>{row.age} days</td>
-                  <td>
-                    <label className="label cursor-pointer">
-                      <input
-                        type="checkbox"
-                        className="toggle"
-                        checked={row.priced} // Use a property to determine the initial checked state
-                        onChange={() => handleCheckboxChange(index, row)}
-                      />
-                    </label>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
-          {/* Display div with labels on smaller screens */}
-          <div className="md:hidden text-sm min-w-full">
-            {storeData.map((row, index) => (
-              <div key={index} className={`mb-4 flex flex-col px-2 ${index % 2 === 0 ? "bg-gray-100" : "bg-white"}`}>
-                <table>
-                  <tbody>
-                    <tr>
+      {storeData.length > 0 ? (
+        <>
+          <div className="flex justify-center mt-4">
+            <form className="flex">
+              <button
+                className={`btn btn-sm md:btn-md btn-outline me-4 ${sortedBy === "cost" ? "btn-active" : null}`}
+                onClick={sortByCost}
+              >
+                Sort By SOH@Cost
+              </button>
+              <button className="btn btn-sm md:btn-md btn-outline me-4 " onClick={sortByAge}>
+                Sort By Age
+              </button>
+              <button
+                className={`btn btn-sm md:btn-md btn-outline me-4 ${sortedBy === "soh" ? "btn-active" : null}`}
+                onClick={sortBySOH}
+              >
+                Sort By SOH
+              </button>
+            </form>
+          </div>
+          <div className="mt-5 px-2 md:px-40">
+            <p className="p-2">{storeData.length} clearance lines</p>
+            <div className="overflow-x-auto">
+              {/* Display table on medium or larger screens */}
+              <table className="hidden md:table table-zebra-zebra">
+                <thead>
+                  <tr>
+                    <th>Article</th>
+                    <th>Description</th>
+                    <th>MAP</th>
+                    <th>SOH@Cost</th>
+                    <th>RRP</th>
+                    <th>Z-Status</th>
+                    <th>SOH</th>
+                    <th>Age</th>
+                    <th>Priced</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {storeData.map((row, index) => (
+                    <tr key={index}>
                       <td>{row.article}</td>
-                    </tr>
-                    <tr>
-                      <td className="font-bold" colSpan={5}>
-                        {row.description}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td style={{ width: "15%" }}>MAP:</td>
-                      <td style={{ width: "20%" }}>${CurrencyFormatter.format(row.map)}</td>
-                      <td style={{ width: "15%" }}>Status:</td>
+                      <td>{row.description}</td>
+                      <td>{CurrencyFormatter.format(row.map)}</td>
+                      <td>{CurrencyFormatter.format(row.cost)}</td>
+                      <td>{CurrencyFormatter.format(row.rrp)}</td>
                       <td>{row.z_status}</td>
-                      <td>Priced</td>
-                    </tr>
-                    <tr>
-                      <td>SOH@Cost:</td>
-                      <td>${CurrencyFormatter.format(row.cost)}</td>
-                      <td style={{ width: "20%" }}>SOH:</td>
                       <td>{row.soh}</td>
+                      <td>{row.age} days</td>
                       <td>
                         <label className="label cursor-pointer">
                           <input
@@ -166,19 +128,67 @@ export const StockInfo = ({ storeData, setStoreData, selectedDepartment }: Store
                         </label>
                       </td>
                     </tr>
-                    <tr>
-                      <td>RRP:</td>
-                      <td>${row.rrp}</td>
-                      <td>Age:</td>
-                      <td>{row.age} days</td>
-                    </tr>
-                  </tbody>
-                </table>
+                  ))}
+                </tbody>
+              </table>
+
+              {/* Display div with labels on smaller screens */}
+              <div className="md:hidden text-sm min-w-full">
+                {storeData.map((row, index) => (
+                  <div
+                    key={index}
+                    className={`mb-4 flex flex-col px-2 ${index % 2 === 0 ? "bg-gray-100" : "bg-white"}`}
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>{row.article}</td>
+                        </tr>
+                        <tr>
+                          <td className="font-bold" colSpan={5}>
+                            {row.description}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style={{ width: "15%" }}>MAP:</td>
+                          <td style={{ width: "20%" }}>${CurrencyFormatter.format(row.map)}</td>
+                          <td style={{ width: "15%" }}>Status:</td>
+                          <td>{row.z_status}</td>
+                          <td>Priced</td>
+                        </tr>
+                        <tr>
+                          <td>SOH@Cost:</td>
+                          <td>${CurrencyFormatter.format(row.cost)}</td>
+                          <td style={{ width: "20%" }}>SOH:</td>
+                          <td>{row.soh}</td>
+                          <td>
+                            <label className="label cursor-pointer">
+                              <input
+                                type="checkbox"
+                                className="toggle"
+                                checked={row.priced} // Use a property to determine the initial checked state
+                                onChange={() => handleCheckboxChange(index, row)}
+                              />
+                            </label>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>RRP:</td>
+                          <td>${row.rrp}</td>
+                          <td>Age:</td>
+                          <td>{row.age} days</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <LoadingPage />
+      )}
     </>
   );
 };
